ca6766f52787072ff6a5502551dcabddf40d5a36
--list <--list>
2025-07-10 14:22:23 +0700

    update

--- decode.py
+++ decode.py
@@ -1,13 +1,13 @@
 from openlocationcode import openlocationcode as olc
 
 # Plus code
-plus_code = "HP63+3G"
+plus_code = "HM5W+V7" 
 
 # Harus ditambahkan area referensi (misalnya kota atau provinsi)
-area_context = "Pagerwojo, Sidoarjo, Indonesia"
+area_context = "Jati, Sidoarjo Regency, East Java"
 
 # Decode dengan asumsi di Sidoarjo
-decoded = olc.decode(plus_code)
+decoded = olc.decode(olc.recoverNearest(plus_code, -7.45, 112.70))
 
 print(f"Latitude: {decoded.latitudeCenter}")
 print(f"Longitude: {decoded.longitudeCenter}")
